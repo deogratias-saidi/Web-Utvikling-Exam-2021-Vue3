e3de024bba882ee893fa1a71401b99788e263d00
Deogratias Djuma Saidi <50410476+deogratias-saidi@users.noreply.github.com>
2021-05-06 23:21:36 +0200

    last update

--- findamusician-font-end/src/components/Admin/RequestItems.vue
+++ findamusician-font-end/src/components/Admin/RequestItems.vue
@@ -18,7 +18,7 @@
             <tr v-for="request in requests" :key="request.id">
               <td>{{ request.id }}</td>
               <td>{{ request.userName }}</td>
-              <td>{{ dateFilter(request.dateCreated)  }}</td>
+              <td>{{ dateFilter(request.dateCreated) }}</td>
               <td>NOK {{ request.budget }}</td>
               <td class="d-flex justify-content-center gap-3">
                 <router-link
@@ -27,8 +27,9 @@
                   ><i class="bx bx-show"></i
                 ></router-link>
                 <div
-                  class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop"
-                  
+                  class="btn btn-danger"
+                  data-bs-target="#staticBackdrop"
+                  v-on:click="deleteRequest(request.id)"
                 >
                   <i class="bx bx-trash"></i>
                 </div>
@@ -39,63 +40,36 @@
       </div>
     </div>
   </div>
-
-  <!-- Modal -->
-
-  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer" v-for="request in requests" :key="request.id">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" v-on:click="deleteRequest(request.id)" >Understood</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-
 </template>
 
 <script>
-import axios from 'axios'
-import moment from 'moment'
+import axios from "axios";
+import moment from "moment";
 export default {
-    data(){
+  data() {
     return {
-      requests: []
-    }
-
+      requests: [],
+    };
   },
   methods: {
-
-    getRequests(){
-      axios.get("https://localhost:5001/api/requests/")
-      .then((res) => {
-        this.requests = res.data
-      })
+    getRequests() {
+      axios.get("https://localhost:5001/api/requests/").then((res) => {
+        this.requests = res.data;
+      });
     },
 
-    deleteRequest(id){
-      axios.delete(`https://localhost:5001/api/requests/${id}`)
-      .then(() => {
-        this.getRequests()
-      })
+    deleteRequest(id) {
+      axios.delete(`https://localhost:5001/api/requests/${id}`).then(() => {
+        this.getRequests();
+      });
     },
-    dateFilter(dateCreated){
-        return moment(dateCreated).format('DD/MM/YYYY')
-    }
-    
+    dateFilter(dateCreated) {
+      return moment(dateCreated).format("DD/MM/YYYY");
+    },
   },
-  mounted(){
-    this.getRequests()
-  }
+  mounted() {
+    this.getRequests();
+  },
 };
 </script>
 
